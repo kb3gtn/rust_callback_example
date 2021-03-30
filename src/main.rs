143fcd3a2ad226bd2eb618c7bb65b5b2c195c318
Peter Fetterer <kb3gtn@gmail.com>
2021-03-29 22:47:54 -0400

    elaborated on usage and added comments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
 pub mod my_mod;
 
+fn custom_rx_cb(ci : &my_mod::ClientInfo ) -> std::result::Result<my_mod::CBResult, std::io::Error> {
+    println!("custom rx callback for client_id {}.", ci.client_id );
+    Ok(my_mod::CBResult::Keep)
+}
+
+fn custom_rx2_cb(ci : &my_mod::ClientInfo ) -> std::result::Result<my_mod::CBResult, std::io::Error> {
+    println!("custom rx2 callback for client_id {}.", ci.client_id );
+    Ok(my_mod::CBResult::Keep)
+}
+
+
+fn  custom_tx_cb(ci : &my_mod::ClientInfo ) -> std::result::Result<my_mod::CBResult, std::io::Error> {
+    println!("custom tx callback for client_id {}.", ci.client_id );
+    Ok(my_mod::CBResult::Keep)
+}
+
 fn main() {
     println!("callback test.");
+
+    // create an instance of the client info struct to use
     let ci = my_mod::ClientInfo {
         client_fd : 5,
         client_id : 1,
     };
 
-    let cbs = my_mod::CallbackStruct::new();
+    // create a instance of the callback structure initialized with 
+    // default callbacks
+    println!("Default callbacks............");
+    let default_cbs = my_mod::CallbackStruct::new();
+    default_cbs.on_conn(&ci).unwrap();
+    default_cbs.on_rx(&ci).unwrap();
+    default_cbs.on_tx(&ci).unwrap();
+    default_cbs.on_dcon(&ci).unwrap();
 
-    cbs.on_conn(&ci).unwrap();
-    cbs.on_rx(&ci).unwrap();
-    cbs.on_tx(&ci).unwrap();
-    cbs.on_dcon(&ci).unwrap();
+    println!("Custom callbacks..............");
+    // create an instance with custom callbacks for tx and rx
+    let mut custom_cbs = my_mod::CallbackStruct {
+        rx_cb : (custom_rx_cb),
+        tx_cb : (custom_tx_cb),
+        conn_cb : (my_mod::default_conn_cb),
+        dcon_cb : (my_mod::default_dcon_cb),
+    };
+
+    custom_cbs.on_conn(&ci).unwrap();
+    custom_cbs.on_rx(&ci).unwrap();
+    custom_cbs.on_tx(&ci).unwrap();
+    custom_cbs.on_dcon(&ci).unwrap();
+
+    // change mapping on the fly.
+    println!("update rx_cb to rx2 callback......");
+    custom_cbs.rx_cb = custom_rx2_cb;
+    custom_cbs.on_rx(&ci).unwrap();
+
+    println!("done..");
+
 
 }
--- src/my_mod.rs
+++ src/my_mod.rs
@@ -1,37 +1,46 @@
+// Module to hold custom code
 
+// public structure used by callback functions
 pub struct ClientInfo {
     pub client_fd   : i32,
     pub client_id   : u64,
 }
 
+// made up result enum for callbacks to return
 pub enum CBResult {
     Keep,
     Disconnect,
 }
-///////////////// default callbacks ////////////////
-fn default_rx_cb(ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
-    println!("rx callback for client_id {}.", ci.client_id );
+
+/// Define default callbacks to use, when CallbackStruct is made using new
+pub fn default_rx_cb(ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
+    println!("default rx callback for client_id {}.", ci.client_id );
     Ok(CBResult::Keep)
 }
-fn default_tx_cb(ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
-    println!("tx callback for client_id {}.", ci.client_id );
+pub fn default_tx_cb(ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
+    println!("default tx callback for client_id {}.", ci.client_id );
     Ok(CBResult::Keep)
 }
-fn default_conn_cb(ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
-    println!("conn callback for client_id {}.", ci.client_id );
+pub fn default_conn_cb(ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
+    println!("default conn callback for client_id {}.", ci.client_id );
     Ok(CBResult::Keep)
 }
-fn default_dcon_cb(ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
-    println!("dconn callback for client_id {}.", ci.client_id );
+pub fn default_dcon_cb(ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
+    println!("default dconn callback for client_id {}.", ci.client_id );
     Ok(CBResult::Disconnect)
 }
+
+// structure of callback function pointers
 pub struct CallbackStruct {
     pub rx_cb         : fn(ci : &ClientInfo) -> std::result::Result<CBResult, std::io::Error>,
     pub tx_cb         : fn(ci : &ClientInfo) -> std::result::Result<CBResult, std::io::Error>,
     pub conn_cb       : fn(ci : &ClientInfo) -> std::result::Result<CBResult, std::io::Error>,
     pub dcon_cb       : fn(ci : &ClientInfo) -> std::result::Result<CBResult, std::io::Error>,
 }
+
+// implementation of methods for Callback Structure
 impl CallbackStruct {
+    // create a new default CallbackStruct instance.
     pub fn new() -> CallbackStruct {
         CallbackStruct {
             rx_cb : (default_rx_cb),
@@ -40,16 +49,21 @@ impl CallbackStruct {
             dcon_cb : (default_dcon_cb),
         }
     }
+    // interface to call the rx_cb callback for this structure
     pub fn on_rx(&self, ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
         (self.rx_cb)(&ci)
     }
+    // interface to call the tx_cb callback for this structure
     pub fn on_tx(&self, ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
         (self.tx_cb)(&ci)
     }
+    // interface to call the conn_cb callback for this structure
     pub fn on_conn(&self, ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
         (self.conn_cb)(&ci)
     }
+    // interface to call the dcon_cb callback for this structure.
     pub fn on_dcon(&self, ci : &ClientInfo ) -> std::result::Result<CBResult, std::io::Error> {
         (self.dcon_cb)(&ci)
     }
 }
+
